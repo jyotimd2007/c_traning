27b55f8b2b2f4d982b074245284d655c136b28eb
jyotimd2007 <jyotimd2007@gmail.com>
2025-11-22 10:01:30 +0530

    Add files via upload

---
+++ scoreptr.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+struct score{
+    float marks;
+    int rank;
+};
+void edit_ref(struct score *s1)
+{
+    printf("before change in fn : %f\n",s1->marks);
+    s1->marks=34.9;
+    printf("after change in fn :%f\n",s1->marks);
+}
+void main()
+{
+    struct score s1={87.9,4};
+    edit_ref(&s1);
+    printf("after change in main %f\n",s1.marks);
+
+
+}
